662c4c7fcf19cc2ef894959cb3c8f44e870ea028
luciniculaie <103644484+luciniculaie@users.noreply.github.com>
2022-12-11 23:50:53 +0200

    Add files via upload

---
+++ why-not-backend/src/main/java/com/whynot/whynotbackend/WhyNotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.whynot.whynotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyNotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyNotBackendApplication.class, args);
+	}
+
+}
---
+++ why-not-backend/src/test/java/com/whynot/whynotbackend/WhyNotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whynot.whynotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyNotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
